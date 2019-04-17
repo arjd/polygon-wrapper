1da70b24c3b65e7fa75fe3239e89a2f4c25f7798
Adam Dornford <adornford@formatherapeutics.com>
2019-04-17 09:21:07 -0400

    reasonable limits so we don't flood polygon.io

--- src/Polygon.ts
+++ src/Polygon.ts
@@ -1,4 +1,19 @@
-import axios from "axios";
+import superagent from 'superagent';
+import { SuperAgentRequest } from 'superagent';
+import Throttle from 'superagent-throttle'; // FIXME full type declarations for these modules
+import RetryDelay from 'superagent-retry-delay';
+
+RetryDelay(superagent);
+const throttler = new Throttle({
+  active: true,
+  rate: 100,
+  ratePer: 1000,
+  concurrent: 2,
+});
+
+const request = (s: any): SuperAgentRequest => {
+  return s.retry(2, 5000).use(throttler.plugin());
+}
 
 interface QueryParameters {
   [key: string]: string | boolean | number;
@@ -25,7 +40,7 @@ export default class polygon {
   protected async get<T>(uri: string, ...params: Array<QueryParameters>): Promise<T> {
     var obj: QueryParameters = { apiKey: this.apiKey };
     const url = `${this.baseUrl}${uri}${paramsToQuery(Object.assign(obj, params))}`;
-    const responseP = axios.get(url).then(res => res.data);
+    const responseP = request(superagent.get(url)).then(res => res.body);
 
     return responseP;
   }
